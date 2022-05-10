a746180899b0ad36ac09c5bad2e1635be857bc12
Marcus Vinicius de Faria Santos <marcusv_fs@hotmail.com>
2022-05-10 15:39:54 -0300

    Fé no Pai

--- Jogo/sobre.py
+++ Jogo/sobre.py
@@ -1,31 +1,3 @@
 from tkinter import *
 from menu import *
 
-def criar_sobre():
-    tela = Tk()
-    tela.geometry('1920x1080')
-    tela.title('GATE: O RESGATE')
-    tela['bg']='#5d8a82'
-    tela.attributes('-fullscreen',True)
-    f = ("Times bold", 14)
-
-    def Voltar2():
-        tela.destroy()
-        criar_menu()
-
-    Label(
-        tela,
-        text="Life is short do what you want",
-        padx=20,
-        pady=20,
-        bg='#ffbf00',
-        font=f
-    ).pack(expand=True, fill=BOTH)
-
-    Button(
-        tela, 
-        text="Voltar", 
-        font=f,
-        command=Voltar2
-        ).pack(fill=X, expand=TRUE, side=LEFT)
-    tela.mainloop()
--- Jogo/tl_garagem.py
+++ Jogo/tl_garagem.py
@@ -2,10 +2,10 @@ from tkinter import *
 from tl_sala import *
 from tl_bomb import *
 import os, sys #necessária para as funções de write e read
-from fcntl import ioctl
+#from fcntl import ioctl
 from ioctl_cmds import *#defines para iotctl
 
-fd = os.open("/dev/mydev", os.O_RDWR)
+#fd = os.open("/dev/mydev", os.O_RDWR)
 
 def bomb():
     os.close(fd)
